57d2862b974a3ab1f7ed04795695a6a85a725aac
khj0426 <706shin1728@naver.com>
2022-11-16 10:45:17 +0900

    feat:App에 Store컴포넌트 가져와서 추가

--- Kimhyojung/intorducefood/src/App.tsx
+++ Kimhyojung/intorducefood/src/App.tsx
@@ -1,7 +1,72 @@
 import React from 'react';
-
+import Store from './components/Store';
+import GlobalStyle from './GlobalStyle';
+import { Fragment } from 'react';
+import Store1Img from './Store1.jpg';
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <GlobalStyle></GlobalStyle>
+      <h1>나만의 맛집 소개</h1>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <h1>재관님의 맛집 소개</h1>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+      <Store
+        name="바스버거"
+        address="서울특별시 중구 무교동 11 광일빌딩 지하 1층"
+        imgSrc={Store1Img}
+        mainfood="바스버거,더블베이컨치즈버거"
+        imgAlt="햄버거최고"
+      ></Store>
+    </Fragment>
+  );
 }
 
 export default App;
