2e2c8133d01a030dca4b5cbf2b1b62f128d9ab8a
sinamong0620 <sinaskim60@gmail.com>
2022-11-16 14:16:53 +0900

    feat: 맛집 제목, 주소, 대표메뉴 추가

--- kimsina/src/components/StoreTexting.tsx
+++ kimsina/src/components/StoreTexting.tsx
@@ -4,10 +4,18 @@ import StoreTextingDetail from "./StoreTextingDetail";
 const StoreTexting = () => {
   return (
     <StoreTextingContainerCss>
-      <StoreTextingDetail />
-      <StoreTextingDetail />
-      <StoreTextingDetail />
-      <StoreTextingDetail />
+      <StoreTextingDetail
+        title="부천 상무초밥"
+        address="미야옹"
+        menu="미야옹"
+      />
+      <StoreTextingDetail
+        title="의정부 오뎅식당"
+        address="미야옹"
+        menu="미야옹"
+      />
+      <StoreTextingDetail title="토끼정" address="미야옹" menu="미야옹" />
+      <StoreTextingDetail title="롯데리아(?)" address="미야옹" menu="미야옹" />
     </StoreTextingContainerCss>
   );
 };
--- kimsina/src/components/StoreTextingDetail.tsx
+++ kimsina/src/components/StoreTextingDetail.tsx
@@ -1,13 +1,37 @@
 import styled from "styled-components";
 
-const StoreTextingDetail = () => {
-  return <StoreTextingDetailCircle></StoreTextingDetailCircle>;
+interface Props {
+  title: String;
+  address: String;
+  menu: String;
+}
+const StoreTextingDetail = ({ title, address, menu }: Props) => {
+  return (
+    <StoreTextingDetailCircle>
+      <StoreTextingDetailSpan>{title}</StoreTextingDetailSpan>
+      <StoreTextingDetailp>{address}</StoreTextingDetailp>
+      <StoreTextingDetailp>{menu}</StoreTextingDetailp>
+    </StoreTextingDetailCircle>
+  );
 };
 
 export default StoreTextingDetail;
 
 const StoreTextingDetailCircle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   width: 12em;
   height: 15em;
+  padding: 1em;
   box-shadow: 0.5em 0.5em 1em #c8c8c8;
+`;
+
+const StoreTextingDetailSpan = styled.span`
+  font-size: 1.2em;
+`;
+
+const StoreTextingDetailp = styled.p`
+  font-size: 0.8em;
 `;
