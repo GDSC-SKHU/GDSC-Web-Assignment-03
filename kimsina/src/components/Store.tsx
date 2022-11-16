abcd2d26243bd341752f5f0ab7822614416edbfc
sinamong0620 <sinaskim60@gmail.com>
2022-11-16 10:33:34 +0900

    feat: StoreCircle & Store.tsx 파일 추가

---
+++ kimsina/src/components/Store.tsx
@@ -0,0 +1,10 @@
+import StoreCircle from "./StoreCircle";
+const Store = () => {
+  return (
+    <div className="Store">
+      <StoreCircle />
+    </div>
+  );
+};
+
+export default Store;
---
+++ kimsina/src/components/StoreCircle.tsx
@@ -0,0 +1,8 @@
+const StoreCircle = () => {
+  return (
+    <div>
+      <div className="menu_circle"></div>
+    </div>
+  );
+};
+export default StoreCircle;
